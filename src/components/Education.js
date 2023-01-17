d5ce365e0baa4d36ab9689687e784626e28a325a
Mit20S <mitsawant@deloitte.com>
2023-01-17 17:03:27 +0530

    education page update

---
+++ src/components/Education.js
@@ -0,0 +1,49 @@
+import variables from "../styles/variables.module.css";
+// import useStyles from "../styles/app.module.css";
+import styles from "../styles/education.module.css";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+function Education(){
+    // Declaring the constant url
+    const url = "http://localhost:3000/education/";
+
+    // Declaring Education array
+    const [EducationArray, setEducation] = useState([]);
+    
+    useEffect(()=>{getEducation()}, [EducationArray]);
+
+    // Defining getEducation Function
+    function getEducation(){
+         
+        axios.get(url).then(resData => {
+            setEducation(resData.data);
+        })
+    }
+
+    // Iterating the Education Array
+    let result = EducationArray.map((item,index)=>
+    <div key={index} className={styles.eduBlock}>
+        <p className={styles.subTitle}>Course: {item.course}</p>
+        <p className={styles.bodyContent}>Institution: {item.institution}</p>
+        <p className={styles.bodyContent}>Location: {item.location}</p>
+        <p className={styles.bodyContent}>Year of Passing: {item.pass_year}</p>
+    </div>)
+
+    return(
+        <>
+            <div className={styles.mainHeading}>
+               <h1 className={styles.mainTitle}>Education</h1>
+            </div>
+            {/* <hr/> */}
+            <div className={styles.mainBlock}>
+                {result}
+            </div>
+        </>
+    );
+}
+export default Education;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+
+import Education from './components/Education';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Education />
   </React.StrictMode>
 );
 
