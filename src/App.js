a32c7fe603690dbc169b28f1cbb79b7e2ac7e63c
MadZeke7 <erenji@deloitte.com>
2023-01-17 12:37:19 +0530

    structured skeleton for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import variables from "./styles/variables.module.css";
+import styles from "./styles/app.module.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>YO MFERRR</h1>
     </div>
   );
 }
