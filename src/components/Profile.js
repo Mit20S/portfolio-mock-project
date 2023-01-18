839900786e048e557b0189601a5703f1225a11ce
MadZeke7 <erenji@deloitte.com>
2023-01-18 22:00:20 +0530

    social component added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,17 +2,23 @@ import Header from "../components/Header";
 import Img from "../assets/personal.jpg";
 import styles from "../styles/profile.module.css"
 
+import { AiFillGithub, AiFillFacebook , AiFillTwitterCircle, AiFillInstagram} from 'react-icons/ai'
+
 function Profile(){
     return(
         <section>
             <Header />
             <section className={styles.imgDesc}>
-                <img src={Img}/>
-                <p>I am a dedicated, organized and methodical individual. I have good interpersonal skills, am an excellent team worker and am keen and very willing to learn and develop new skills. I am reliable and dependable and often seek new responsibilities within a wide range of employment areas. I have an active and dynamic approach to work and getting things done. I am determined and decisive. I identify and develop opportunities.</p>
+                <img src={Img} alt="personal"/>
+                <p>A <span>dedicated</span>, <span>organised</span> and <span>methodical</span> individual who has good interpersonal skills, is an excellent team worker and is very willing to learn and develop new skills. I am <span>reliable</span> and <span>dependable</span> and often seek new responsibilities within a wide range of employment areas. I have an <span>active</span> and dynamic approach to work and getting things done.</p>
             </section>
             <section className={styles.social}>
-                <div>
-                    
+                <h1>Socials</h1>
+                <div className={styles.icons}>
+                    <a target="blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><AiFillGithub /> </a>
+                    <a target="blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><AiFillFacebook /> </a>
+                    <a target="blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><AiFillTwitterCircle /> </a>
+                    <a target="blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><AiFillInstagram /> </a>
                 </div>
             </section>
         </section>
