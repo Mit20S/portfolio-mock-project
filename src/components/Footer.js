fceb761f6b9ece3341e2dc766ba8e630a2c6b519
MadZeke7 <erenji@deloitte.com>
2023-01-18 23:45:30 +0530

    added contact details to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import variables from "../styles/variables.module.css";
 import styles from "../styles/footer.module.css";
+import { BsTelephoneFill } from "react-icons/bs";
+import {MdOutlineAlternateEmail} from "react-icons/md"
+import { MdLocationPin} from "react-icons/md"
 
 function Footer(){
     return(
         <footer>
             <div className={styles.mainFooter}>
+                <div className={styles.contactDetails}>
+                    <p><BsTelephoneFill /> +9160509695</p>
+                    <p><MdOutlineAlternateEmail /> shlingala@deloitte.com</p>
+                    <p><MdLocationPin />Mumbai, Maharastra</p>
+                </div>
                 <p className={styles.pMargin}>&copy; Copyright 2023 <b> Mumbai Studios Team</b></p>
             </div>
         </footer>
