b4932de423c48d795b5fcbf5d987873c5b9d7b6d
Ankeet2704 <ankanil@deloitte.com>
2023-01-19 17:22:37 +0530

    completed getting resume data

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,6 +8,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GetData from "./GetData";
+import { BsStarFill } from "react-icons/bs";
+import { BsStar } from "react-icons/bs";
 
 function Resume(){
     // Declaring the constant url
@@ -17,41 +19,46 @@ function Resume(){
     const professionalUrl = "http://localhost:3000/professional";
 
     const [profileObj, setprofileObj] = useState({});
-    const [educationObj, seteducationObj] = useState({});
+    const [educationObj, seteducationObj] = useState([]);
     const [projectObj, setprojectObj] = useState([]);
-    const [professionalObj, setprofessionalObj] = useState({});
+    const [skills, setSkills] = useState([]);
+    const [experience, setExperience] = useState([]);
+
+    axios.get(professionalUrl).then( resData => 
+    {
+        setSkills(resData.data.skills);
+        setExperience(resData.data.experience);
+    });
 
     GetData(profileUrl, profileObj, setprofileObj);
     GetData(educationUrl, educationObj, seteducationObj);
     GetData(projectUrl, projectObj, setprojectObj);
-    GetData(professionalUrl, professionalObj, setprofessionalObj);
 
     return(
         <>
             <Header />
             <div className={styles.mainHeading}>
-               <h1 className={styles.mainTitle}>RESUME</h1>
+                <h1 className={styles.mainTitle}>RESUME</h1>
             </div>
 
             <main className={styles.resume}>
                 <div className={styles.nameAndImg}>
-                    <div>
+                    <div className={styles.name}>
                         <h1>{profileObj.name}</h1>
                         <p>{profileObj.deloitte_mail}</p>
                         <p>{profileObj.phone}</p>
-                        <p>{professionalObj.summary}</p>
+                        <br></br>
+                        <p>{profileObj.summary}</p>
                     </div>
                     <div>
                         <img src={Img} alt="personal" />
                     </div>
-
                 </div>
-                <hr />
 
-
-                <section>
-                    <div>
+                <section className={styles.resumeSection}>
+                    <div className={styles.projAndExp}>
                         <div className={styles.projects}>
+                            <h2>Projects</h2>
                             {projectObj.map(( item, index)=>{
                                 return(
                                     <div key={index} className={styles.card}>
@@ -65,8 +72,8 @@ function Resume(){
                         </div>
 
                         <div className={styles.professional}>
-                            {professionalObj.skills}
-                            {/* {professionalObj.experience.map((item,index) =>
+                            <h2>Experience</h2>
+                            {experience.map((item,index) =>
                                 <div key={index} className={styles.expCard}>
                                     <h2>{item.company_name}</h2>
                                     <p><b>Role: </b>{item.role}</p>
@@ -76,12 +83,43 @@ function Resume(){
                                     <hr></hr>
                                     <p>{item.details}</p>
                                 </div>
-                            )} */}
+                            )}
                         </div>
                         {/* Projects and Profession */}
                     </div>
-                    <div>
-                        {/* Education and Skills */}
+
+                    <div className={styles.skillsAndEdu}>
+                        <div className={styles.education}>
+                        <h2>Education</h2>
+                        {educationObj.map(( item, index)=>{
+                        return(
+                            <div key={index} className={styles.eduBlock}>
+                            <p className={styles.subTitle}>Course: {item.course}</p>
+                            <p className={styles.bodyContent}>Institution: {item.institution}</p>
+                            <p className={styles.bodyContent}>Location: {item.location}</p>
+                            <p className={styles.bodyContent}>Year of Passing: {item.pass_year}</p>
+                            </div>
+                            )
+                        })}
+                        </div>
+
+                        <div className={styles.skills}>
+                            <h2>Skills</h2>
+                            {skills.map((item,index) =>
+                            <div key={index} className={styles.skillCard}>
+                                <p>{item.skill_name}</p>
+                                <hr></hr>
+                                <span>
+                                    {[...Array(item.rating)].map((item,index) => 
+                                    <BsStarFill key={index} className={styles.icon}></BsStarFill>
+                                    )}
+                                    {[...Array(5-item.rating)].map((item,index) => 
+                                    <BsStar  key={index} className={styles.icon}></BsStar>
+                                    )}
+                                </span>
+                            </div>
+                            )}
+                        </div>
                     </div>
                 </section>
             </main>
